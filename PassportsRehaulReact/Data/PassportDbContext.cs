f2aa4a2d537aadad7ba94868e0646d2461ee0555
Joseph Ruiz <Ruiz-joseph@hotmail.com>
2023-05-04 15:15:51 -0400

    connect to DB

---
+++ PassportsRehaulReact/Data/PassportDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PassportsRehaulReact.Data
+{
+
+    public class PassportDbContext : DbContext
+    {
+
+        public IConfiguration _config { get; set; }
+
+        public PassportDbContext(IConfiguration config)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer();
+        }
+
+    }
+
+}
