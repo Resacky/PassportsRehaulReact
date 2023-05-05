d895757d64b481b86902a0785078f67e21aca978
Resacky <Ruiz-joseph@hotmail.com>
2023-05-04 20:32:24 -0400

    Revert "pushing this to start from zero at home"
    This reverts commit 5848d4188e352372e177fe46ee4ba2ebd2d4d2e5.

---
+++ PassportsRehaulReact/Migrations/20230504201413_Add_PassPortARSSD_Table.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PassportsRehaulReact.Migrations
+{
+    public partial class Add_PassPortARSSD_Table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PassPortARSSDs",
+                columns: table => new
+                {
+                    PPARSIDSD = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    sort = table.Column<int>(type: "int", nullable: false),
+                    ARSDescription = table.Column<string>(type: "nchar", nullable: false),
+                    ARSFee = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PassPortARSSDs", x => x.PPARSIDSD);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PassPortARSSDs");
+        }
+    }
+}
---
+++ PassportsRehaulReact/Migrations/20230504201936_Add_PassPortLockBoxes_Table.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PassportsRehaulReact.Migrations
+{
+    public partial class Add_PassPortLockBoxes_Table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PassPortLockBoxess",
+                columns: table => new
+                {
+                    LOCKBOXID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LBoxDescription = table.Column<string>(type: "char(50)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PassPortLockBoxess", x => x.LOCKBOXID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PassPortLockBoxess");
+        }
+    }
+}
