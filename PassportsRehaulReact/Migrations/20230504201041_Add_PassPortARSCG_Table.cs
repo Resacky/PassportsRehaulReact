1b3a78d7de09bd9d37379a38ad781544e952852d
Joseph Ruiz <Ruiz-joseph@hotmail.com>
2023-05-04 21:04:31 -0400

    PLS

---
+++ PassportsRehaulReact/Migrations/20230504201041_Add_PassPortARSCG_Table.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PassportsRehaulReact.Migrations
+{
+    public partial class Add_PassPortARSCG_Table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PassPortARSCGs",
+                columns: table => new
+                {
+                    PPARSIDCG = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    sort = table.Column<int>(type: "int", nullable: false),
+                    ARSDescription = table.Column<string>(type: "nchar", nullable: false),
+                    ARSFee = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PassPortARSCGs", x => x.PPARSIDCG);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PassPortARSCGs");
+        }
+    }
+}
---
+++ PassportsRehaulReact/Migrations/20230504201622_Add_PassPortFees_Table.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PassportsRehaulReact.Migrations
+{
+    public partial class Add_PassPortFees_Table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PassPortFeess",
+                columns: table => new
+                {
+                    PPFEEID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    sort = table.Column<int>(type: "int", nullable: false),
+                    FeeDescription = table.Column<string>(type: "varchar(50)", nullable: false),
+                    FeeAmtSD = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    FeeAmtCG = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PassPortFeess", x => x.PPFEEID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PassPortFeess");
+        }
+    }
+}
