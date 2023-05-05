fb701bc4a0883ca019a2588cb835d0c1629be40c
Joseph Ruiz <Ruiz-joseph@hotmail.com>
2023-05-05 15:31:25 -0400

    Added page navigation

--- PassportsRehaulReact/ClientApp/src/index.js
+++ PassportsRehaulReact/ClientApp/src/index.js
@@ -3,8 +3,11 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
-import Template from './pages/Template';
-import DisplayEmployeeTable from './pages/DisplayEmployeeTable';
+/* list of pages */
+import Home from './pages/Home';
+import AddEntry from './pages/AddEntry';
+import SearchAndEditEntry from './pages/SearchAndEditEntry';
+import SelectAReport from './pages/SelectAReport';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
@@ -14,8 +17,10 @@ root.render(
     <>
         <Router>
             <Routes>
-                <Route path="/" element={<Template />} />
-                <Route path="/DisplayEmployeeTable" element={<DisplayEmployeeTable />} />
+                <Route path="/Home" element={<Home />} />
+                <Route path="/AddEntry" element={<AddEntry />} />
+                <Route path="/SearchAndEditEntry" element={<SearchAndEditEntry />} />
+                <Route path="/SelectReporting" element={<SelectAReport />} />
             </Routes>
         </Router>
     </>
