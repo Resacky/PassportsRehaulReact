627362360b76979afe5f52dd86aebd0be1a99b67
Joseph Ruiz <Ruiz-joseph@hotmail.com>
2023-05-08 12:22:41 -0400

    modded AddEntry logic
    modified the AddEntry logic where I am passing in the firstName, middleName, ..., parameters into the component and in turn updating their useState to then use the values as needed accordingly

--- PassportsRehaulReact/ClientApp/src/components/ApplicantInformation.js
+++ PassportsRehaulReact/ClientApp/src/components/ApplicantInformation.js
@@ -1,66 +1,54 @@
-﻿import React, { useState } from 'react';
+﻿import React from 'react';
 
-const ApplicantInformation = () => {
-    const [firstName, setFirstName] = useState('');
-    const [middleName, setMiddleName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [dateOfBirth, setDateOfBirth] = useState('');
-    const [phoneNumber, setPhoneNumber] = useState('');
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        alert(`Name: ${firstName} ${middleName} ${lastName}\nDate of Birth: ${dateOfBirth}\nPhone Number: ${phoneNumber}`);
-    };
-
+const ApplicantInformation = ({
+    firstName, setFirstName,
+    middleName, setMiddleName,
+    lastName, setLastName,
+    dateOfBirth, setDateOfBirth,
+    phoneNumber, setPhoneNumber
+}) => {
     return (
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label htmlFor="firstName">First Name:</label>
-                <input
-                    type="text"
-                    id="firstName"
-                    value={firstName}
-                    onChange={(e) => setFirstName(e.target.value)}
-                />
-            </div>
-            <div>
-                <label htmlFor="middleName">Middle Name:</label>
-                <input
-                    type="text"
-                    id="middleName"
-                    value={middleName}
-                    onChange={(e) => setMiddleName(e.target.value)}
-                />
-            </div>
-            <div>
-                <label htmlFor="lastName">Last Name:</label>
-                <input
-                    type="text"
-                    id="lastName"
-                    value={lastName}
-                    onChange={(e) => setLastName(e.target.value)}
-                />
-            </div>
-            <div>
-                <label htmlFor="dateOfBirth">Date of Birth:</label>
-                <input
-                    type="date"
-                    id="dateOfBirth"
-                    value={dateOfBirth}
-                    onChange={(e) => setDateOfBirth(e.target.value)}
-                />
-            </div>
-            <div>
-                <label htmlFor="phoneNumber">Phone Number:</label>
-                <input
-                    type="tel"
-                    id="phoneNumber"
-                    value={phoneNumber}
-                    onChange={(e) => setPhoneNumber(e.target.value)}
-                />
-            </div>
-            <button type="submit">Submit</button>
-        </form>
+        <div>
+            <label htmlFor="firstName">First Name:</label>
+            <input
+                type="text"
+                id="firstName"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+            />
+            <br />
+            <label htmlFor="middleName">Middle Name:</label>
+            <input
+                type="text"
+                id="middleName"
+                value={middleName}
+                onChange={(e) => setMiddleName(e.target.value)}
+            />
+            <br />
+            <label htmlFor="lastName">Last Name:</label>
+            <input
+                type="text"
+                id="lastName"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+            />
+            <br />
+            <label htmlFor="dateOfBirth">Date of Birth:</label>
+            <input
+                type="date"
+                id="dateOfBirth"
+                value={dateOfBirth}
+                onChange={(e) => setDateOfBirth(e.target.value)}
+            />
+            <br />
+            <label htmlFor="phoneNumber">Phone Number:</label>
+            <input
+                type="tel"
+                id="phoneNumber"
+                value={phoneNumber}
+                onChange={(e) => setPhoneNumber(e.target.value)}
+            />
+        </div>
     );
 };
 
--- PassportsRehaulReact/ClientApp/src/pages/AddEntry.js
+++ PassportsRehaulReact/ClientApp/src/pages/AddEntry.js
@@ -1,4 +1,5 @@
-﻿import OtherPageLinks from "../components/OtherPageLinks";
+﻿import React, { useState } from 'react';
+import OtherPageLinks from "../components/OtherPageLinks";
 import ApplicantInformation from "../components/ApplicantInformation";
 import LockBoxDropdownMenu from "../components/LockBox";
 import TypeOfPassportBox from "../components/TypeOfPassportBox";
@@ -7,18 +8,39 @@ import StateDepartment from "../components/StateDepartment";
 import "../styles/AddEntryStyles/AddEntryStyle.css";
 
 function AddEntry() {
+
+    const [firstName, setFirstName] = useState('');
+    const [middleName, setMiddleName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [dateOfBirth, setDateOfBirth] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(`Name: ${firstName} ${middleName} ${lastName}\nDate of Birth: ${dateOfBirth}\nPhone Number: ${phoneNumber}`);
+    };
+
     return (
         <>
             <div className="mainContainer">
-                <div>
+                <div className="OtherPageLinksContainer">
                     <OtherPageLinks />
                 </div>
-                <div>
-                    <ApplicantInformation />
+                <form onSubmit={handleSubmit}>
+
+                    <ApplicantInformation
+                        firstName={firstName} setFirstName={setFirstName}
+                        middleName={middleName} setMiddleName={setMiddleName}
+                        lastName={lastName} setLastName={setLastName}
+                        dateOfBirth={dateOfBirth} setDateOfBirth={setDateOfBirth}
+                        phoneNumber={phoneNumber} setPhoneNumber={setPhoneNumber}
+                    />
+
                     <LockBoxDropdownMenu />
                     <TypeOfPassportBox />
                     <StateDepartment />
-                </div>
+                    <button type="submit">Submit</button>
+                </form>
             </div>
         </>
     );
