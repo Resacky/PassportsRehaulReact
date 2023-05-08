3c23eb22808e63a29f0b2db4b91f262e66ede2a2
Joseph Ruiz <Ruiz-joseph@hotmail.com>
2023-05-08 16:39:30 -0400

    Added more styling for AddEntry
    Added more styling for the AddEntry page

--- PassportsRehaulReact/ClientApp/src/components/OtherPageLinks.js
+++ PassportsRehaulReact/ClientApp/src/components/OtherPageLinks.js
@@ -2,16 +2,14 @@
 
 function OtherPageLinks() {
     return (
-        <>
-            <div>
-                <ul>
-                    <li><Link to="/Home" className="Links">Pass Port Home</Link></li>
-                    <li><Link to="/AddEntry" className="Links">Add Entry</Link></li>
-                    <li><Link to="/SearchAndEditEntry" className="Links">Search and Edit Entry</Link></li>
-                    <li><Link to="/SelectReporting" className="Links">Select a Report</Link></li>
-                </ul>
-            </div>
-        </>
+        <div>
+            <ul>
+                <li><Link to="/Home" className="Links">Pass Port Home</Link></li>
+                <li><Link to="/AddEntry" className="Links">Add Entry</Link></li>
+                <li><Link to="/SearchAndEditEntry" className="Links">Search and Edit Entry</Link></li>
+                <li><Link to="/SelectReporting" className="Links">Select a Report</Link></li>
+            </ul>
+        </div>
     );
 }
 
--- PassportsRehaulReact/ClientApp/src/components/StateDepartment.js
+++ PassportsRehaulReact/ClientApp/src/components/StateDepartment.js
@@ -11,21 +11,18 @@ const StateDepartment = () => {
     };
 
     return (
-        <div className="StateDepartmentContainer">
-            <label>State Department Fees</label>
-            <br />
-            <label>Passport</label>
-            <br />
-            //This label is for the passport amount dependent on what the user choose for the type of passport // 
-            <label>{isConditionTrue ? 'Text when condition is true' : 'Text when condition is false'}</label>
-            <br />
-            <AddedReturnServicesBox />
-            <br />
-            <label>Dependent to change based on option chosen above</label>
-            <br />
-            <label>Total amount</label>
-            <br />
-            <button onClick={handleButtonClick}>Toggle Condition</button>
+        <div className="SDframe">
+            <div className="Header">
+                <label>State Department Fees</label>
+            </div>
+            <div className="StateDepartmentContainer">
+                <div className="ColumnLabels">
+                    <label>a</label>
+                </div>
+                <div className="InfoLabels">
+                    <label>a</label>
+                </div>
+            </div>
         </div>
     );
 };
