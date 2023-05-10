bb1f5c44327c8b4663c82752256e817bbc8bf455
Joseph Ruiz <Ruiz-joseph@hotmail.com>
2023-05-10 16:17:42 -0400

    added banner and modified it accordingly for home

---
+++ PassportsRehaulReact/ClientApp/src/components/Banner.js
@@ -0,0 +1,26 @@
+﻿import React from 'react';
+import "../styles/Banner.css";
+import CityLogo from "../assets/CityLogoHorizontalWhite.png";
+
+function Banner() {
+
+    return (
+        <>
+            <div class="header">
+                <div class="headerContent">
+                    <div>
+                        <a href="https://www.coralgables.com">
+                            <img src={CityLogo} alt="City Logo" class="cityLogo" />
+                        </a>
+                    </div>
+                    <div>
+                        <a href="https://www.coralgables.com/department/office-city-clerk" class="Clerktext">
+                            <label class="Clerktext"><span>Back To City Clerk Homepage</span></label></a>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Banner;
--- PassportsRehaulReact/ClientApp/src/pages/Home.js
+++ PassportsRehaulReact/ClientApp/src/pages/Home.js
@@ -1,10 +1,12 @@
-﻿import OtherPageLinks from "../components/OtherPageLinks";
+﻿import Banner from '../components/Banner';
+import OtherPageLinks from "../components/OtherPageLinks";
 
 import '../styles/HomeStyle.css';
 
 function Home() {
     return (
         <>
+            <Banner />
             <div className="HomeContainer">
                 <OtherPageLinks Links="HomeLinks" ul="HomeUl" li="HomeLi" LinkStyle="primary" />
             </div>
