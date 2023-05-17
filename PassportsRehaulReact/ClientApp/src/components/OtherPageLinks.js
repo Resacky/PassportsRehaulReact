73a9a0fa6ec3e79c1afeb9a94bae0d136198ae55
Joseph Ruiz <Ruiz-joseph@hotmail.com>
2023-05-17 16:53:17 -0400

    changes the header button names

--- PassportsRehaulReact/ClientApp/src/components/OtherPageLinks.js
+++ PassportsRehaulReact/ClientApp/src/components/OtherPageLinks.js
@@ -10,8 +10,8 @@ function OtherPageLinks({ Links, ul, li, LinkStyle }) {
         <div className={Links}>
             <ul className={ul}>
                 <li className={li}><Link to="/AddEntry" className={LinkStyle}>Add Entry</Link></li>
-                <li className={li}><Link to="/SearchAndEditEntry" className={LinkStyle}>Search and Edit Entry</Link></li>
-                <li className={li}><Link to="/SelectReporting" className={LinkStyle}>Select a Report</Link></li>
+                <li className={li}><Link to="/SearchAndEditEntry" className={LinkStyle}>Search and Edit Entries</Link></li>
+                <li className={li}><Link to="/SelectReporting" className={LinkStyle}>Reporting</Link></li>
             </ul>
         </div>
     );
